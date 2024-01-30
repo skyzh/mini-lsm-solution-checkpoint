251c5f08e8493ed1aab7609cb34222999d902771
Alex Chi Z <iskyzh@gmail.com>
2024-01-30 12:15:24 +0800

    v202401: checkpoint week 3 day 4 solution (#19)
    * v202401: checkpoint week 3 day 4 solution
    
    Signed-off-by: Alex Chi <iskyzh@gmail.com>
    
    * fix bugs
    
    Signed-off-by: Alex Chi <iskyzh@gmail.com>
    
    ---------
    
    Signed-off-by: Alex Chi <iskyzh@gmail.com>

--- src/mvcc.rs
+++ src/mvcc.rs
@@ -2,7 +2,7 @@
 #![allow(dead_code)] // TODO(you): remove this lint after implementing this mod
 
 pub mod txn;
-mod watermark;
+pub mod watermark;
 
 use std::{
     collections::{BTreeMap, HashSet},
@@ -54,8 +54,9 @@ impl LsmMvccInner {
     }
 
     pub fn new_txn(&self, inner: Arc<LsmStorageInner>, serializable: bool) -> Arc<Transaction> {
-        let ts = self.ts.lock();
+        let mut ts = self.ts.lock();
         let read_ts = ts.0;
+        ts.1.add_reader(read_ts);
         Arc::new(Transaction {
             inner,
             read_ts,
--- src/mvcc/watermark.rs
+++ src/mvcc/watermark.rs
@@ -1,6 +1,3 @@
-#![allow(unused_variables)] // TODO(you): remove this lint after implementing this mod
-#![allow(dead_code)] // TODO(you): remove this lint after implementing this mod
-
 use std::collections::BTreeMap;
 
 pub struct Watermark {
@@ -14,11 +11,23 @@ impl Watermark {
         }
     }
 
-    pub fn add_reader(&mut self, ts: u64) {}
+    pub fn add_reader(&mut self, ts: u64) {
+        *self.readers.entry(ts).or_default() += 1;
+    }
 
-    pub fn remove_reader(&mut self, ts: u64) {}
+    pub fn remove_reader(&mut self, ts: u64) {
+        let cnt = self.readers.get_mut(&ts).unwrap();
+        *cnt -= 1;
+        if *cnt == 0 {
+            self.readers.remove(&ts);
+        }
+    }
+
+    pub fn num_retained_snapshots(&self) -> usize {
+        self.readers.len()
+    }
 
     pub fn watermark(&self) -> Option<u64> {
-        Some(0)
+        self.readers.first_key_value().map(|(ts, _)| *ts)
     }
 }
